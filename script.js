29876e54dab1a81a4f30c4170ca42462a5f94577
Lea Rutnik <66997312+rutniklea@users.noreply.github.com>
2024-01-17 10:03:01 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const result = document.getElementById('result')
+const filter = document.getElementById('filter')
+const listItems = []
+
+getData()
+
+filter.addEventListener('input', (e) => filterData(e.target.value))
+
+// represent the response from our request
+async function getData () {
+    const res = await fetch ('https://randomuser.me/api?results=100')
+
+    // need to call json to get our data
+    const { results } = await res.json()
+
+
+    // clear results
+    result.innerHTML = ''
+
+    results.forEach(user => {
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+li.innerHTML = `
+  <img src="${user.picture.large}" alt="${user.name.first}">
+  <div class="user-info">
+    <h4>${user.name.first}, ${user.name.last}</h4>
+    <p>${user.location.city}, ${user.location.country}</p>
+    <p>${user.dob.age}</p>
+  </div>
+`;
+
+        result.appendChild(li)
+    })
+}
+
+function filterData (searchTerm) {
+    listItems.forEach( item => {
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+            item.classList.remove('hide')
+        } else {
+            item.classList.add('hide')
+        }
+    })
+}
